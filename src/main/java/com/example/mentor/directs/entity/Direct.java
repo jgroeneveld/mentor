263d1a0a24af908adb5f81cbda8c82ea15ae0a6d
Jaap Groeneveld <jgroeneveld@fastmail.com>
2018-07-25 14:50:29 +0200

    add getters to direct

--- src/main/java/com/example/mentor/directs/entity/Direct.java
+++ src/main/java/com/example/mentor/directs/entity/Direct.java
@@ -12,4 +12,13 @@ public class Direct {
 
     private String firstName;
     private String lastName;
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
 }
