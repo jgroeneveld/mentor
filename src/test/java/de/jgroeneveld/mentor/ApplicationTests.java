81e16a3bc50ed31e75085b5ddbf666faba81256d
Jaap Groeneveld <jgroeneveld@fastmail.com>
2018-07-31 19:50:03 +0200

    rename DemoApplication to Application

--- src/test/java/de/jgroeneveld/mentor/ApplicationTests.java
+++ src/test/java/de/jgroeneveld/mentor/ApplicationTests.java
@@ -9,7 +9,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 @RunWith(SpringRunner.class)
 @SpringBootTest
 @ActiveProfiles("test")
-public class DemoApplicationTests {
+public class ApplicationTests {
 
     @Test
     public void contextLoads() {
